4af4a6372b4724070ceaf8d47a8af6c4c44933e1
Ghouse <ghousethanedar@gmail.com>
2020-09-04 14:06:34 +0530

    utill files added

--- utills/dataset/loaddataset.py
+++ utills/dataset/loaddataset.py
@@ -3,20 +3,41 @@ import cv2
 import numpy as np
 
 
-class LoadDataset:
-    def __int__(self, preprocessor=None):
-        self.preprocessor = preprocessor
-        if self.preprocessor is None:
-            self.preprocessor = []
+class SimpleDatasetLoader:
+    # Method: Constructor
+    def __init__(self, preprocessors=None):
+        """
+        :param preprocessors: List of image preprocessors
+        """
+        self.preprocessors = preprocessors
 
-    def load(self, imagepaths, verbose=-1):
+        if self.preprocessors is None:
+            self.preprocessors = []
+
+    # Method: Used to load a list of images for pre-processing
+    def load(self, image_paths, verbose=-1):
+        """
+        :param image_paths: List of image paths
+        :param verbose: Parameter for printing information to console
+        :return: Tuple of data and labels
+        """
         data, labels = [], []
-        for i, imagepath in enumerate(imagepaths):
-            image = cv2.imread(imagepath)
-            label = imagepath.split(os.path.spe)[-2]
-            if self.preprocessor is not None:
-                for p in self.preprocessor:
+
+        for i, image_path in enumerate(image_paths):
+            image = cv2.imread(image_path)
+            label = image_path.split(os.path.sep)[-2]
+
+            if self.preprocessors is not None:
+                for p in self.preprocessors:
                     image = p.preprocess(image)
+
             data.append(image)
             labels.append(label)
-        return ( np.array(data), np.array(labels) )
+
+            if verbose > 0 and i > 0 and (i+1) % verbose == 0:
+                print('[INFO]: Processed {}/{}'.format(i+1, len(image_paths)))
+
+        return (np.array(data), np.array(labels))
+
+
+
